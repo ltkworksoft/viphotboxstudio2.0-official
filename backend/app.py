c1f53e964f5608de54db0f8e51baccbf310ab533
Triple S Mafia <sx3mafia@gmail.com>
2022-09-27 22:38:22 -0400

    update booking

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, session
+import os
+from flask import Flask, session, request, redirect
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
